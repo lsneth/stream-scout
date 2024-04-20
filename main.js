873fd7174b84d844ea25b3496a5e24d0b953970a
Lucas <luke@nethercott.com>
2024-04-20 06:52:07 -0600

    initialize next

--- main.js
+++
@@ -1,8 +0,0 @@
-import displayShows from './displayResults.js'
-
-const searchForm = document.getElementById('search-form')
-searchForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-  displayShows(e.target.showType.value, e.target.searchQuery.value)
-  document.getElementById('hero').remove()
-})
