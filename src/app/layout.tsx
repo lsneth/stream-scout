ac7a13624843de3e4d59d9f46ea870a91e3a72f6
Lucas <luke@nethercott.com>
2024-05-04 12:03:52 -0400

    poppins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,13 @@ import type { Metadata } from 'next'
 import './globals.css'
 import NavBar from './NavBar'
 import Footer from './Footer'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'Stream Scout',
@@ -15,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className={`${poppins.className} h-full`}>
       <body className="flex h-full flex-col bg-home-design bg-cover text-center text-white">
         <NavBar />
         <div className="grow bg-gradient-to-t from-black">{children}</div>
