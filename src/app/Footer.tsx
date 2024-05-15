5a15fa5e98ca911a9bc6e911b6eaa1a90622f6aa
Lucas Nethercott <luke@nethercott.com>
2024-05-15 15:11:35 -0600

    add link to privacy policy in footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -5,12 +5,15 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="flex min-h-12 justify-between bg-black p-4">
-      <Link
+      <a
         target="_blank"
         href="https://developer.themoviedb.org/reference/intro/getting-started"
         className="flex items-center"
       >
         <Image src="/tmdb.svg" alt="tmdb logo" height={24} width={96} />
+      </a>
+      <Link href="/privacy-policy" className="text-xs">
+        Privacy Policy
       </Link>
       <a href="https://www.lucasnethercott.com" className="text-xs">
         ©2024 Lucas Nethercott
