28981be794f59cf20dcd774ff9cf2f1d6a0450e4
Lucas Nethercott <luke@nethercott.com>
2024-05-16 09:23:55 -0600

    add just watch logo to footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -5,19 +5,35 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="flex min-h-12 justify-between bg-black p-4">
-      <a
-        target="_blank"
-        href="https://developer.themoviedb.org/reference/intro/getting-started"
-        className="flex items-center"
-      >
-        <Image src="/tmdb.svg" alt="tmdb logo" height={24} width={96} />
-      </a>
-      <Link href="/privacy-policy" className="text-xs">
-        Privacy Policy
-      </Link>
-      <a href="https://www.lucasnethercott.com" className="text-xs">
-        ©2024 Lucas Nethercott
-      </a>
+      <div className="flex gap-10">
+        <a
+          target="_blank"
+          href="https://developer.themoviedb.org/reference/intro/getting-started"
+          className="flex items-center"
+        >
+          <Image src="/tmdb.svg" alt="tmdb logo" height={24} width={96} />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.justwatch.com/"
+          className="flex items-center"
+        >
+          <Image
+            src="/just-watch.webp"
+            alt="just watch logo"
+            height={24}
+            width={96}
+          />
+        </a>
+      </div>
+      <div className="flex gap-10">
+        <Link href="/privacy-policy" className="text-xs">
+          Privacy Policy
+        </Link>
+        <a href="https://www.lucasnethercott.com" className="text-xs">
+          ©2024 Lucas Nethercott
+        </a>
+      </div>
     </div>
   )
 }
